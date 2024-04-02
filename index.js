1a2978d77d941c04b3f24e25fa6514e656f6d8f5
Guang <jouyang19@proton.me>
2024-04-01 21:27:25 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(name, gift) {
+    const cards = [];
+    for (let i = 0; i < name.length; i++) {
+        cards[i] = `Thank you, ${name[i]}, for the wonderful ${gift} gift!`;
+    }
+    
+    console.log(cards);
+
+    return cards;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
